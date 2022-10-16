a457222ecb7992d8f1b94932c678930a5128a47a
LEEGWONSEOK <leestart1023@gmail.com>
2022-10-16 13:07:27 +0900

    Feat: 2번호출 일어나는 상황 해결

--- src/app.js
+++ src/app.js
@@ -7,7 +7,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-app.use("/", (req, res) => {
+/* 호출 방지 */
+app.get("/favicon.ico", (req, res) => res.status(204));
+
+app.use("/", (req, res, next) => {
+  console.log("middleware 1");
+  next();
+});
+
+app.use((req, res) => {
+  console.log("middleware 2");
   res.send("hello, express");
 });
 
