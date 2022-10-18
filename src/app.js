8ed67d6fee22da74a6a8f0263ea121f3d3cdf14a
LEEGWONSEOK <leestart1023@gmail.com>
2022-10-18 15:37:54 +0900

    Feat: 패키지 모듈 불러오기 & Router 분리 작업 진행

--- src/app.js
+++ src/app.js
@@ -1,24 +1,19 @@
 // @ts-check
 
-/* eslint-disable no-console */
-
 const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const routes = require("./routes");
 
+dotenv.config();
 const app = express();
+app.use(morgan("dev"));
 const PORT = 8080;
 
 /* 호출 방지 */
 app.get("/favicon.ico", (req, res) => res.status(204));
 
-app.use("/", (req, res, next) => {
-  console.log("middleware 1");
-  next();
-});
-
-app.use((req, res) => {
-  console.log("middleware 2");
-  res.send("hello, express");
-});
+app.use("/api", routes);
 
 app.listen(PORT, () => {
   console.log(`The Express server is listening at port : ${PORT}`);
---
+++ src/routes/users.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+const express = require("express");
+
+const router = express.Router({ mergeParams: true });
+
+router.get("/", (req, res) => {
+  res.send("users router");
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  console.log("is work?");
+  res.send(`users router ${id}`);
+});
+
+module.exports = router;
