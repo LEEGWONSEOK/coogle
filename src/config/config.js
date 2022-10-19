40ed4d8a77e84e1821253bc2049e4da7611ff03d
LEEGWONSEOK <leestart1023@gmail.com>
2022-10-19 21:06:18 +0900

    Chore: sequelize config 파일 생성 완료

---
+++ src/config/config.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const { env } = process;
+
+module.exports = {
+  development: {
+    username: env.DB_USERNAME,
+    password: env.DB_PASSWORD,
+    database: env.DB_DATABASE,
+    host: env.DB_HOST,
+    dialect: 'mysql',
+    logging: false,
+  },
+};
