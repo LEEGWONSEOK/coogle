ef554fdf561ebc71ec8578c6e008afd712770b11
LEEGWONSEOK <leestart1023@gmail.com>
2022-10-19 21:00:43 +0900

    Feat: sequelize 서버에 연결 완료

--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
 // @ts-check
 
+/* module import */
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const routes = require('./routes');
+const { sequelize } = require('./models');
+
+const PORT = 8080;
+const app = express();
+const { log, error } = console;
 
 dotenv.config();
-const app = express();
 app.use(morgan('dev'));
-const PORT = 8080;
+
+sequelize
+  .sync({ force: true }) // force: true >> 테이블 다 지우고 다시 생성(데이터 다 날아감, 실무X) / alter: true >> 데이터 유지 가능
+  .then(() => {
+    log('✅ DB connect!');
+  })
+  .catch((err) => {
+    error(err);
+  });
 
 /* 호출 방지 */
 app.get('/favicon.ico', (req, res) => res.status(204));
@@ -16,5 +29,5 @@ app.get('/favicon.ico', (req, res) => res.status(204));
 app.use('/api', routes);
 
 app.listen(PORT, () => {
-  console.log(`The Express server is listening at port : ${PORT}`);
+  log(`The Express server is listening at port : ${PORT}`);
 });
