601f154ba09226fb1086818992847fe1b8cb50e7
LEEGWONSEOK <leestart1023@gmail.com>
2022-12-04 22:56:01 +0900

    Feat: 메인 스크립트 TS에 맞춰서 변경

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,29 @@
-/* module import */
-import express from 'express';
+/* Module import */
+import express, { Request, Response, NextFunction } from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import hpp from 'hpp';
 import helmet from 'helmet';
-import routes from './routes';
+import indexRoutes from './routes';
 import { sequelize } from './models';
 
 dotenv.config();
+
+/* APP variables */
 const { env } = process;
-const port = env.PORT;
-const app = express();
+const PORT: number = parseInt(env.PORT as string) || 8080;
+const HOST: string = env.HOST || 'localhost';
+const app: express.Application = express();
 
+/* APP configuration */
 app.use(morgan('dev'));
+app.use(cors());
 
+/* Sequelize-DB connect test */
 sequelize
-  .sync({ force: true })
+  .sync({ force: false })
   .then(() => {
     console.log('DB 연결을 성공했습니다.');
   })
@@ -30,7 +36,7 @@ sequelize
 app.get('/favicon.ico', (req, res) => res.status(204));
 
 /* API 라우터 */
-app.use('/api', routes);
+app.use('/api', indexRoutes);
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: '??연결이 잘 되었는지?' });
@@ -45,6 +51,6 @@ app.get('*', (req, res) => {
 // jenkins test2
 
 /* Server 연결 */
-app.listen(port, () => {
-  console.log(`Express Server가 포트 '${port}'에서 실행중입니다.`);
+app.listen(PORT, HOST, () => {
+  console.log(`Express Server가 포트 '${HOST}:${PORT}'에서 실행중입니다.`);
 });
